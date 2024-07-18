2b440f9a6f5e50a1a22e129a4eb43b5fbd4b769d
Varun <44650484+varunhhhrahul@users.noreply.github.com>
2024-07-18 12:45:12 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ module.exports = {
 
   // #database
   MONGO_URI:
-    "mongodb+srv://varun:WntBLcPjyQmaxJ7N@memories-toggle.g3nwo.mongodb.net/memories-toggle?retryWrites=true&w=majority",
+    "",
 
-  JWT_SECRET: "oihwoehfihiefhoheowhfdsfdfsaffdsa",
+  JWT_SECRET: "",
   JWT_EXPIRE: "30d",
   JWT_COOKIE_EXPIRE: 30,
   FAST2SMS:
-    "2YJua1cxeDgT0blZQrRBnp4MiVwX8t6CAH37oGN9hyvkmqdPKOEqosrB2LeQNmYukygvh5l6VTFjJ1KO",
+    "",
 };
